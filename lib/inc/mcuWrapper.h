a64adca06447e065dd94501f546d79eec6508f3e
Th2will <wni1717@gmail.com>
2024-01-16 10:43:11 -0800

    ¯\_(ツ)_/¯

--- lib/inc/mcuWrapper.h
+++ lib/inc/mcuWrapper.h
@@ -31,16 +31,7 @@ and its licensor.
 // #include "stm32f4xx_hal.h"
 // #include "stm32f4xx_hal_tim.h"
 // #include "stm32f4xx_it.h"
-
-extern ADC_HandleTypeDef hadc1;         /* Mcu dependent ADC handler */
-extern I2C_HandleTypeDef hi2c1;         /* Mcu dependent I2C handler */
-extern I2C_HandleTypeDef hi2c3;         /* Mcu dependent I2C handler */
-extern SPI_HandleTypeDef hspi1;         /* Mcu dependent SPI handler */
-extern SPI_HandleTypeDef hspi5;         /* Mcu dependent SPI handler */
-extern UART_HandleTypeDef huart4;       /* Mcu dependent UART handler */
-extern UART_HandleTypeDef huart5;       /* Mcu dependent UART handler */
-extern TIM_HandleTypeDef htim2;         /* Mcu dependent TIM handler */
-extern TIM_HandleTypeDef htim5;         /* Mcu dependent TIM handler */
+/* Mcu dependent TIM handler */
 
 #define CS_PIN ARDUINO_GPIO10_Pin               /* Mcu dependent chip select */
 #define GPIO_PORT ARDUINO_GPIO10_GPIO_Port      /* Mcu dependent adc chip select port */
