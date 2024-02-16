36aa2125a2cfd2209bafe92af8977dc407aead98
Th2will <wni1717@gmail.com>
2024-02-16 00:07:41 -0800

    added test cases to be verifiable

--- src/main.cpp
+++ src/main.cpp
@@ -14,9 +14,15 @@ void wakeBms();
 // Object declarations 
 //isoSPI isoSPI1(&SPI, 10, 8, 7, 9, 5, 6, 4, 3, 2);
 //isoSPI isoSPI2(&SPI1, 0, 25, 24, 33, 29, 28, 30, 31, 32);
+enum test_case {VOLTAGE, CAN, FAN, GPIO, TEENSY};
+test_case debug = VOLTAGE;
 
 CANLine can;
+short message[8] = {60000,4,0,0,0,0,0,0};
+std::vector<byte> pong;
+
 cell_asic IC[TOTAL_IC];
+
 fanController fans(&Serial8);
 bool test_bool[10] = {0,0,0,0,0,0,0,0,0,1};
 
@@ -26,14 +32,47 @@ void setup() {
   adBms6830_init_config(TOTAL_IC, &IC[0]);
   
 }
-std::vector<byte> pong;
-cell_asic test;
-void loop() {
 
-  adBms6830_start_adc_cell_voltage_measurment(TOTAL_IC);
-  //for some reason this doesn't work, why not?
-  adBms6830_read_cell_voltages(TOTAL_IC, &IC[0]);
-  adBms6830_read_cell_voltages(TOTAL_IC, &IC[1]);
+void loop() {
+  switch (debug)
+  {
+  case VOLTAGE:
+    //expected result: print out the cell voltages
+    adBms6830_start_adc_cell_voltage_measurment(TOTAL_IC);
+    //for some reason this doesn't work, why not?
+    adBms6830_read_cell_voltages(TOTAL_IC, &IC[0]);
+    adBms6830_read_cell_voltages(TOTAL_IC, &IC[1]);
+    break;
+
+  case CAN:  
+      //sends Precharge stuff to VDM, expects a response back of some kind
+      can.send(97, message);
+      can.recieve_one();
+      pong = can.recieve(97);
+      break;
+
+  case FAN:
+    //dunno yet
+    break;
+
+  case GPIO:
+    adbms6830_write_gpio(TOTAL_IC, &IC[0], test_bool);
+    //start aux voltage measurement sets all the GPIO pins to low, this is adjustable in the code
+    adBms6830_start_aux_voltage_measurment(TOTAL_IC, &IC[0]);
+    adBms6830_read_aux_voltages(TOTAL_IC, &IC[0]);
+    break;
+
+  case TEENSY:
+    Serial.println("Teensy is probably not the issue");
+    break;
+
+  default:
+    Serial.println("Uh oh u dummy u didn't set what to debug");
+    delay(1000);
+    break;
+  }
+
+  
   
 }
 
