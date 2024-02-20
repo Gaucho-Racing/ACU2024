df481aa7f275cce5da4473eb37d9a347659ecbb5
Th2will <wni1717@gmail.com>
2024-02-19 17:55:33 -0800

    f-ed up fix? for PWM

--- src/main.cpp
+++ src/main.cpp
@@ -13,11 +13,13 @@ void wakeBms();
 // Object declarations 
 //isoSPI isoSPI1(&SPI, 10, 8, 7, 9, 5, 6, 4, 3, 2);
 //isoSPI isoSPI2(&SPI1, 0, 25, 24, 33, 29, 28, 30, 31, 32);
-enum test_case {VOLTAGE, CAN, FAN, GPIO, TEENSY, CELLBAL};
+enum test_case {VOLTAGE, CAN, FAN, GPIO, TEENSY, CELLBAL, EXTRA};
 test_case debug = VOLTAGE;
 
+uint8_t Wrpwm1[2]        = { 0x00, 0x20 };
+uint8_t Wrpwm2[2]        = { 0x00, 0x21 };
 CANLine can;
-short message[8] = {60000,4,0,0,0,0,0,0};
+unsigned int short message[8] = {60000,4,0,0,0,0,0,0};
 std::vector<byte> pong;
 
 cell_asic IC[TOTAL_IC];
@@ -30,7 +32,6 @@ void setup() {
   // fans.begin();
   adBms6830_init_config(TOTAL_IC, &IC[0]);
   adBms6830_init_config(TOTAL_IC, &IC[1]);
-  
 }
 
 void loop() {
@@ -68,7 +69,12 @@ void loop() {
 
   case CELLBAL:
     //test discharge first
-    SetPwmDutyCycle(TOTAL_IC, &IC[0], PWM_46_2_PCT);
+    SetPwmDutyCycle(TOTAL_IC, &IC[0], PWM_100_0_PCT);
+    adBms6830CreatePwma(TOTAL_IC, &IC[0]);
+    adBms6830CreatePwmb(TOTAL_IC, &IC[0]);
+    adBmsWriteData(TOTAL_IC, &IC[0], Wrpwm1, Pwm, AA);
+    adBmsWriteData(TOTAL_IC, &IC[0], Wrpwm2, Pwm, BB);
+
     break;
   default:
     Serial.println("Uh oh u dummy u didn't set what to debug");
