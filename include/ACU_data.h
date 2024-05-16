a50ccf8442e5385e318657b39aea92380274d736
Th2will <williamni@ucsb.edu>
2024-05-16 13:05:30 -0700

    ADC testing code initial

--- include/ACU_data.h
+++ include/ACU_data.h
@@ -8,3 +8,13 @@
 //Charger Values
 #define CHARGER_VOLTAGE 540 //V
 #define CHARGER_CURRENT 5 //A
+
+
+#define ADC_MUX_GLV_VOLT 7
+#define ADC_MUX_HV_VOLT 6
+#define ADC_MUX_HV_CURRENT 0
+#define ADC_MUX_SHDN_POW 1
+#define ADC_MUX_DCDC_CURRENT 5
+#define ADC_MUX_DCDC_TEMP1 2
+#define ADC_MUX_DCDC_TEMP2 3
+#define ADC_MUX_FAN_REF 4
--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include "FanController.h"
 #include "ACU_data.h"
 #include "can.cpp"
+#include "ADC1283.h"
 #include <unordered_map>
 
 // put function declarations here:
@@ -16,11 +17,13 @@ float V2T(float voltage, float B = 4390);
 // Object declarations 
 //isoSPI isoSPI1(&SPI, 10, 8, 7, 9, 5, 6, 4, 3, 2);
 //isoSPI isoSPI2(&SPI1, 0, 25, 24, 33, 29, 28, 30, 31, 32);
-enum test_case {VOLTAGE, CAN, FAN, GPIO, TEENSY, CELLBAL, THERMAL, EXTENDEDCELLBAL, EXTRA};
+enum test_case {VOLTAGE, CAN, FAN, GPIO, TEENSY, CELLBAL, THERMAL, EXTENDEDCELLBAL, EXTRA, ADC};
 test_case debug = VOLTAGE;
+
 CANLine can;
 short message[8] = {60000,4,0,0,0,0,0,0};
 std::vector<byte> pong;
+
 float temp[8][2];
 uint16_t cell_to_mux[8] ={0b0011111111, 0b0000111111, 0b0001111111, 0b0010111111, 0b0100111111, 0b0110111111, 0b0111111111, 0b0101111111};
 // std::unordered_map<u_int8_t, u_int16_t> cell_to_mux;
@@ -29,6 +32,9 @@ unsigned long previousMillis = 0;
 cell_asic IC[TOTAL_IC];
 
 fanController fans(&Serial8);
+
+ADC1283 acu_adc;
+
 bool test_bool[10] = {0,0,0,0,0,0,0,0,0,0};
 
 uint8_t Wrpwm1[2] = { 0x00, 0x20 };
@@ -45,7 +51,11 @@ void setup() {
   Serial.println("Setup done");
   //isoSPI1.begin();
   //isoSPI1.setIntFunc(intrFunc);
-
+  acu_adc.begin();
+  //TODO: 
+  acu_adc.setCsPin(10);
+  acu_adc.setFsck(13);
+  acu_adc.setVref(5.0);
 }
 
 void loop() {
@@ -128,6 +138,15 @@ void loop() {
 
     delay(1000);
     break;
+  case ADC:
+    Serial.printf("ADC GLV Voltage: %f\n", acu_adc.readVoltage(ADC_MUX_GLV_VOLT));
+    Serial.printf("ADC HV Voltage: %f\n", acu_adc.readVoltage(ADC_MUX_HV_VOLT));
+    Serial.printf("ADC HV Current: %f\n", acu_adc.readVoltage(ADC_MUX_HV_CURRENT));
+    Serial.printf("ADC Shutdown Power: %f\n", acu_adc.readVoltage(ADC_MUX_SHDN_POW))*4;
+    Serial.printf("ADC DCDC Current: %f\n", acu_adc.readVoltage(ADC_MUX_DCDC_CURRENT));
+    Serial.printf("ADC DCDC Temp1: %f\n", V2T(acu_adc.readVoltage(ADC_MUX_DCDC_TEMP1)));
+    Serial.printf("ADC DCDC Temp2: %f\n", V2T(acu_adc.readVoltage(ADC_MUX_DCDC_TEMP2)));
+    Serial.printf("ADC Fan Ref: %f\n", acu_adc.readVoltage(ADC_MUX_FAN_REF)*2);
   default:
     Serial.println("Uh oh u dummy u didn't set what to debug");
     break;
