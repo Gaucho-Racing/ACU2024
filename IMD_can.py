8d355acccf85f85464adf56d0bd78c5e5ae5ddab
Th2will <williamni@ucsb.edu>
2024-12-19 14:43:00 -0800

    made things a lil nicer

--- IMD_can.py
+++ IMD_can.py
@@ -1,9 +1,11 @@
 #priority is 6 for cyclic, 7 for single shot, see iso175 data sheet for how these are generated
 def calc_can_id(pgn, priority=7):
     source_address = 244
-    id = (priority << 26) + (pgn << 8) + source_address  
-    print("id: " + hex(id))
+    id = (priority << 26) + (pgn << 8) + source_address
+    return id
+    print("id: " + hex(id).upper())
 
-calc_can_id(61184)
+#for decimal: int("<num>"", 16)
 
-    
+id = calc_can_id(61184)
+print("id (hex): " + hex(id).upper() + "\n id (dec): " + str(id))
